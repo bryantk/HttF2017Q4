81cc3fbd5467b53bb63121006c751002d2074dde
Melissa Edge <melissa.edge@imaginelearning.com>
2017-11-16 11:04:05 -0700

    PlayerRig - add HUD

---
+++ HttF2017Q4/Assets/Scripts/HUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void InventoryButtonOnClick(string buttonName)
+	{
+		Debug.Log("Inventory button clicked: " + buttonName);
+		//todo
+	}
+
+	public void EmoticonButtonOnClick(string buttonName)
+	{
+		Debug.Log("Emoticon button clicked: " + buttonName);
+		//todo
+	}
+}
