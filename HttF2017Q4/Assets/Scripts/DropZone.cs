b21295ae2b025d54641544c2d77ae4c0b50f0f99
IMAGINELEARNING\micah.west <micah.west@imaginelearning.com>
2017-11-16 10:28:17 -0700

    Primitive drop zones

---
+++ HttF2017Q4/Assets/Scripts/DropZone.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour
+{
+    [SerializeField] private Color _inactiveColor;
+    [SerializeField] private Color _activeColor;
+    [TextArea]
+    [SerializeField] private string _defaultText = "\n...\n";
+
+    [SerializeField]
+    private LayerMask _itemLayer;
+
+    private bool _isActive;
+    private string _currentText;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _currentText = _defaultText;
+	    SetActive((false));
+	}
+
+    void SetActive(bool active)
+    {
+        foreach (Renderer rend in GetComponentsInChildren<Renderer>())
+        {
+            rend.material.color = active ? _activeColor : _inactiveColor;
+        }
+       
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<TextItem>())
+        {
+            _currentText = col.GetComponent<TextItem>().Text;
+            SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        _currentText = _defaultText;
+        SetActive(false);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (GNM.Instance.IsClient)
+	    {
+	        return;
+	    }
+	}
+
+    public string GetCurrentText()
+    {
+        return _currentText;
+    }
+
+    public float GetSize()
+    {
+        return GetComponent<Renderer>().bounds.size.z;
+    }
+
+    public void SetSize(float size)
+    {
+        //adjust for absolute size
+        float multiplier = GetComponent<Collider>().bounds.size.z / transform.localScale.z;
+        float adjustedSize = size * multiplier;
+        Transform myParent = transform.parent;
+        transform.parent = null;
+        transform.localScale = new Vector3(adjustedSize, adjustedSize, adjustedSize);
+        transform.parent = myParent;
+    }
+}
---
+++ HttF2017Q4/Assets/Scripts/TextItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextItem : MonoBehaviour
+{
+    [TextArea]
+    public string Text;
+
+}
