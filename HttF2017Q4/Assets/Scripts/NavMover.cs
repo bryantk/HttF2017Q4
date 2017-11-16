836c7b85e18c0f1aaab25f83c3af6c39495469ee
IMAGINELEARNING\micah.west <micah.west@imaginelearning.com>
2017-11-16 07:36:26 -0700

    Changed asset serialization to Force Text. Hope we aren't too broken.

---
+++ HttF2017Q4/Assets/Scripts/NavMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMover : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _navAgent;
+
+    [SerializeField] private Transform destination;
+
+    public void MoveToLocation(Vector3 location)
+    {
+        _navAgent.SetDestination(location);
+    }
+
+    void Start()
+    {
+        MoveToLocation(destination.position);
+    }
+}
